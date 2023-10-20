da4cca1ad36a5937bff9e43d76a90fa68dc7b24b
tiniscule <114822071+tiniscule@users.noreply.github.com>
2023-10-20 09:42:44 -0500

    service_role not service_user

--- supabase_test_helpers--0.0.2.sql
+++ supabase_test_helpers--0.0.2.sql
@@ -177,7 +177,7 @@ CREATE OR REPLACE FUNCTION tests.authenticate_as_service_role ()
     RETURNS void
     AS $$
         BEGIN
-            perform set_config('role', 'service_user', true);
+            perform set_config('role', 'service_role', true);
             perform set_config('request.jwt.claims', null, true);
         END
     $$ LANGUAGE plpgsql;

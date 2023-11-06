b8dcb1439d1c43cd1bd3871636909f2bdbfb8e4b
probablykabari <kabari@gmail.com>
2023-11-06 15:00:50 -0600

    Adding app metadata to setup and claims
    - raw_app_meta_data column should have a value on creation
    - app_metadata is populated in the jwt.claims setting

---
+++ supabase_test_helpers--0.0.2--0.0.3.sql
@@ -0,0 +1,91 @@
+/**
+    * ### tests.create_supabase_user(identifier text, email text, phone text)
+    *
+    * Creates a new user in the `auth.users` table.
+    * You can recall a user's info by using `tests.get_supabase_user(identifier text)`.
+    *
+    * Parameters:
+    * - `identifier` - A unique identifier for the user. We recommend you keep it memorable like "test_owner" or "test_member"
+    * - `email` - (Optional) The email address of the user
+    * - `phone` - (Optional) The phone number of the user
+    * - `metadata` - (Optional) Additional user metadata to be added to the user
+    *
+    * Returns:
+    * - `user_id` - The UUID of the user in the `auth.users` table
+    *
+    * Example:
+    * ```sql
+    *   SELECT tests.create_supabase_user('test_owner');
+    *   SELECT tests.create_supabase_user('test_member', 'member@test.com', '555-555-5555');
+    *   SELECT tests.create_supabase_user('test_member', 'member@test.com', '555-555-5555', '{"key": "value"}'::jsonb);
+    * ```
+ */
+CREATE OR REPLACE FUNCTION tests.create_supabase_user(identifier text, email text default null, phone text default null, metadata jsonb default null)
+RETURNS uuid
+    SECURITY DEFINER
+    SET search_path = auth, pg_temp
+AS $$
+DECLARE
+    user_id uuid;
+BEGIN
+
+    -- create the user
+    user_id := extensions.uuid_generate_v4();
+    INSERT INTO auth.users (id, email, phone, raw_user_meta_data, raw_app_meta_data)
+    VALUES (user_id, coalesce(email, concat(user_id, '@test.com')), phone, jsonb_build_object('test_identifier', identifier) || coalesce(metadata, '{}'::jsonb), '{}'::jsonb)
+    RETURNING id INTO user_id;
+
+    RETURN user_id;
+END;
+$$ LANGUAGE plpgsql;
+
+
+/**
+    * ### tests.authenticate_as(identifier text)
+    *   Authenticates as a user created with `tests.create_supabase_user`.
+    *
+    * Parameters:
+    * - `identifier` - The unique identifier for the user
+    *
+    * Returns:
+    * - `void`
+    *
+    * Example:
+    * ```sql
+    *   SELECT tests.create_supabase_user('test_owner');
+    *   SELECT tests.authenticate_as('test_owner');
+    * ```
+ */
+CREATE OR REPLACE FUNCTION tests.authenticate_as (identifier text)
+    RETURNS void
+    AS $$
+        DECLARE
+                user_data json;
+                original_auth_data text;
+        BEGIN
+            -- store the request.jwt.claims in a variable in case we need it
+            original_auth_data := current_setting('request.jwt.claims', true);
+            user_data := tests.get_supabase_user(identifier);
+
+            if user_data is null OR user_data ->> 'id' IS NULL then
+                RAISE EXCEPTION 'User with identifier % not found', identifier;
+            end if;
+
+
+            perform set_config('role', 'authenticated', true);
+            perform set_config('request.jwt.claims', json_build_object(
+                'sub', user_data ->> 'id', 
+                'email', user_data ->> 'email', 
+                'phone', user_data ->> 'phone', 
+                'user_metadata', user_data -> 'raw_user_meta_data', 
+                'app_metadata', user_data -> 'raw_app_meta_data'
+            )::text, true);
+
+        EXCEPTION
+            -- revert back to original auth data
+            WHEN OTHERS THEN
+                set local role authenticated;
+                set local "request.jwt.claims" to original_auth_data;
+                RAISE;
+        END
+    $$ LANGUAGE plpgsql;
--- tests/03-authenticate-as.sql
+++ tests/03-authenticate-as.sql
@@ -1,7 +1,7 @@
 BEGIN;
 CREATE EXTENSION supabase_test_helpers;
 
-select plan(7);
+select plan(9);
 
 select tests.create_supabase_user('test');
 
@@ -10,10 +10,20 @@ select lives_ok($$ select tests.authenticate_as('test') $$, 'Successfully authen
 
 select is((select tests.get_supabase_uid('test')), auth.uid(), 'Authenticates as the correct user');
 select is((select current_role::text), 'authenticated', 'Sets the current role to authenticated');
+select is(
+	current_setting('request.jwt.claims')::jsonb ?& array['sub', 'email', 'phone', 'user_metadata', 'app_metadata'], 
+  true,
+	'Claims should contain correct keys for authenticated user'
+	);
 
 select lives_ok($$ select tests.clear_authentication() $$, 'should clear authentication');
 select is((select current_role::text), 'anon', 'Sets the current role to anonymous');
 select is((select auth.uid()), null, 'Clears out authentication');
+select is(
+	current_setting('request.jwt.claims'), 
+	null, 
+	'Empties request claims'
+	);
 
 select * from finish();
 ROLLBACK;

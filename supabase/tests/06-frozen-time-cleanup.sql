2e5fc7f531135ce85cc8c298670be819fcab40ff
tiniscule <114822071+tiniscule@users.noreply.github.com>
2023-12-10 17:31:28 -0700

    Additional check for confirming no test_overrides in search path

--- supabase/tests/06-frozen-time-cleanup.sql
+++ supabase/tests/06-frozen-time-cleanup.sql
@@ -2,7 +2,7 @@
 BEGIN;
     CREATE EXTENSION supabase_test_helpers;
     
-    select plan(1);
+    select plan(2);
 
     -- freeze the time
     SELECT tests.freeze_time('2020-01-01 00:00:00');
@@ -12,6 +12,12 @@ BEGIN;
         (SELECT search_path_setting_function()),
         (SELECT pg_catalog.now()),
         'function still returns the non frozen time'
+    );
+
+    -- confirm test_overrides no longer in search path
+    select ok(
+        (SELECT current_setting('search_path') NOT LIKE 'test_overrides,%'),
+        'test_overrides no longer in search path'
     );
 
     SELECT * FROM finish();
